49c9e73ca57f9f0a50f1d80deb1c43341c19d143
ROGER PEREIRA SOARES <05046290006@senacrs.edu.br>
2026-09-01 21:10:00 -0300

    Add verificacoes

--- interSearch.py
+++ interSearch.py
@@ -1,20 +1,21 @@
-# import csv
+import csv
 
-# numeros = []
+numeros = []
 
-# with open("./datasets/numeros_1M_ordenado.csv", mode="r") as arq:
-#   dados_csv = csv.DictReader(arq)
-#   for linha in dados_csv:
-#     numeros.append(int(linha['numero']))
+with open("./datasets/numeros_1M_ordenado.csv", mode="r") as arq:
+  dados_csv = csv.DictReader(arq)
+  for linha in dados_csv:
+    numeros.append(int(linha['numero']))
 
-# # pos = primeira_pos + ((alvo - lista[primeira_pos]) * (ultima_pos - primeira_pos)) // (lista[ultima_pos] - lista[low])
-
-numero = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+teste = []
 
 def interSearch(lista, alvo):
     primeira_pos = 0
     ultima_pos = len(lista) - 1
 
+    if lista is None or len(lista) == 0:
+        print("A lista está vazia ou nula.")
+        return -1
     while primeira_pos <= ultima_pos and alvo >= lista[primeira_pos] and alvo <= lista[ultima_pos]:
         if primeira_pos == ultima_pos:
             if lista[primeira_pos] == alvo:
@@ -23,8 +24,6 @@ def interSearch(lista, alvo):
 
         pos = primeira_pos + ((alvo - lista[primeira_pos]) * (ultima_pos - primeira_pos)) // (lista[ultima_pos] - lista[primeira_pos])
         # 0 + ((70 - 10) * (8 - 0)) // (90 - 10)
-
-
         if lista[pos] == alvo:
             return pos
         if lista[pos] < alvo:
@@ -32,6 +31,9 @@ def interSearch(lista, alvo):
         else:
             ultima_pos = pos - 1
 
+    if alvo not in lista:
+        print(f"O número {alvo} não está presente na lista.")
     return -1
-executando = interSearch(numero, 70)
+    
+executando = interSearch(numeros, 585707)
 print(f"Executando a busca Interpolation Search: {executando}")
